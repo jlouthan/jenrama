cb2498b9a5f604be04cd19777e53ea47c7a71d23
Rachel Protacio <rachelprotacio@nat-oitwireless-inside-vapornet100-c-17725.Princeton.EDU>
2018-04-13 14:38:19 -0400

    Maven setup

---
+++ sparrrow-app/src/main/java/edu/princeton/sparrrow/App.java
@@ -0,0 +1,13 @@
+package edu.princeton.sparrrow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
