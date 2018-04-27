35d064f2840a380dc95bc23227291b76d1dd15d2
Jennifer Louthan <jenny@uncorkedstudios.com>
2018-04-26 22:48:09 -0400

    Scheduler tracks tasks results for each job

--- sparrrow-app/src/main/java/edu/princeton/sparrrow/Scheduler.java
+++ sparrrow-app/src/main/java/edu/princeton/sparrrow/Scheduler.java
@@ -128,12 +128,16 @@ public class Scheduler implements Runnable {
         private int frontendId;
         private LinkedList<String> tasksRemaining;
         private ArrayList<String> taskResults;
+        private int numTasks;
 
         // Create a new job with no task results yet
         public Job(int frontendId, Collection<String> tasksRemaining) {
             this.frontendId = frontendId;
             this.tasksRemaining = (LinkedList) tasksRemaining;
             this.taskResults = new ArrayList<String>();
+            //numTasks = tasksRemaining.size();
+            // TODO use above line instead once executor can process multiple tasks per job
+            numTasks = 1;
         }
 
         public String getNextTaskRemaining() {
@@ -142,15 +146,20 @@ public class Scheduler implements Runnable {
             }
             return tasksRemaining.removeFirst();
         }
+
+        public boolean isComplete() {
+            // currently there is no validation that the task results are for unique tasks
+            return taskResults.size() == numTasks;
+        }
     }
 
     private void receivedJob(JobSpecContent m) throws IOException{
-        //Store some state about the job, including remaining tasks to schedule
+        // Store some state about the job, including remaining tasks to schedule
         Job j = new Job(m.getFrontendID(), m.getTasks());
         jobs.put(m.getJobID(), j);
 
-        //Send reservations (probes) to node monitor
-        for (int i = 0; i < m.getTasks().size(); i++) {
+        // Send reservations (probes) to node monitor. Currently sending all tasks to one monitor.
+        for (int i = 0; i < j.tasksRemaining.size(); i++) {
             log("received job spec from Frontend, sending probe to NodeMonitor");
             ProbeContent probe = new ProbeContent(m.getJobID(), this.id);
             Message probeMessage = new Message(MessageType.PROBE, probe);
@@ -161,7 +170,6 @@ public class Scheduler implements Runnable {
     }
 
     private void receivedSpecRequest(ProbeReplyContent m) throws IOException {
-
         // Find the job containing the requested task spec
         UUID jobId = m.getJobID();
         String task = jobs.get(jobId).getNextTaskRemaining();
@@ -178,16 +186,19 @@ public class Scheduler implements Runnable {
     }
 
     private void receivedResult(TaskResultContent m) throws IOException{
-        // TODO: collect task result, return if it's done
+        // collect task result
+        UUID jobId = m.getJobID();
+        Job job = jobs.get(jobId);
+        job.taskResults.add(m.getResult());
 
-        // For now, just pass it back up to the Frontend
-        log("received task result from NodeMonitor, sending job result to Frontend");
-        ArrayList<String> results = new ArrayList<String>();
-        results.add(m.getResult());
-        JobResultContent newMessage = new JobResultContent(m.getJobID(), results);
+        // if task is finished, return result to frontend
+        if (job.isComplete()) {
+            log("received task result from NodeMonitor, sending job result to Frontend");
+            JobResultContent newMessage = new JobResultContent(jobId, job.taskResults);
 
-        Message reply = new Message(MessageType.JOB_RESULT, newMessage);
-        objToFe.writeObject(reply);
+            Message reply = new Message(MessageType.JOB_RESULT, newMessage);
+            objToFe.writeObject(reply);
+        }
     }
 
 }
