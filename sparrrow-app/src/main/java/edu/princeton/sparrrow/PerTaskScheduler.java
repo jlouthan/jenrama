d48b07e469e7bb6c1b034a758cad3638564591a5
Matt Myers <m@myersmatthew.com>
2018-05-07 13:55:44 -0400

    Fixed bugs in PerTaskScheduler

--- sparrrow-app/src/main/java/edu/princeton/sparrrow/PerTaskScheduler.java
+++ sparrrow-app/src/main/java/edu/princeton/sparrrow/PerTaskScheduler.java
@@ -23,16 +23,21 @@ public class PerTaskScheduler extends Scheduler {
 
             String[] taskSpecs = (String[]) m.getTasks().toArray();
 
+            Job job = new Job(m.getFrontendID(), m.getTasks());
+            jobs.put(m.getJobID(), job);
+            log(id + " received job spec from Frontend");
+
             ArrayList<Integer> myMonitors;
             UUID taskId;
             PendingTask pt;
-            for (int i = 0; i < taskSpecs.length; i++) {
+            int i, j;
+            for (i = 0; i < taskSpecs.length; i++) {
                 // Randomize node monitor ids to choose which to place tasks on
                 Collections.shuffle(monitorIds);
 
                 // Select d monitors to sample for this task
                 myMonitors = new ArrayList<>();
-                for(i = 0; i < d; i++){
+                for(j = 0; j < d; j++){
                     myMonitors.add(monitorIds.get(d % monitorIds.size()));
                 }
 
@@ -42,7 +47,7 @@ public class PerTaskScheduler extends Scheduler {
                 Message probeMessage = new Message(MessageType.PROBE, probe);
 
                 // Store task UUID -> PendingTask in hashmap so that we can find the job later
-                pt = new PendingTask(taskId, m.getJobID(), d, taskSpecs[i], myMonitors);
+                pt = new PendingTask(taskId, m.getJobID(), taskSpecs[i], myMonitors);
                 pendingTasks.put(taskId, pt);
 
                 for(int monitorId : myMonitors){
@@ -50,8 +55,6 @@ public class PerTaskScheduler extends Scheduler {
                     objToNodeMonitors.get(monitorId).writeObject(probeMessage);
                 }
             }
-
-
         }
 
         @Override
@@ -70,6 +73,9 @@ public class PerTaskScheduler extends Scheduler {
 
             // Store the value that the node monitor gave for its queue for this task
             myTask.replies[myTask.myMonitors.indexOf(monitorId)] = mes.getqLength();
+            if(myTask.numReplies >= d){
+                super.log("ERROR: recieved too many probe replies for task " + taskId);
+            }
             myTask.numReplies++;
 
             // If all our probes have returned, give the task to the best monitor
@@ -80,14 +86,14 @@ public class PerTaskScheduler extends Scheduler {
                 int i;
                 int bestMonitor = -1;
                 int bestQ = Integer.MAX_VALUE;
-                for(i = 0; i < myTask.d; i++){
+                for(i = 0; i < d; i++){
                     if (myTask.replies[i] < bestQ){
                         bestMonitor = myTask.myMonitors.get(i);
                     }
                 }
 
                 // Create task spec to pass to node monitor
-                taskSpec = new TaskSpecContent(jobId, UUID.randomUUID(), this.id, taskSpecStr);
+                taskSpec = new TaskSpecContent(jobId, taskId, this.id, taskSpecStr);
 
                 // Send the task to my favorite monitor
                 Message spec = new Message(MessageType.TASK_SPEC, taskSpec);
@@ -98,17 +104,15 @@ public class PerTaskScheduler extends Scheduler {
         private class PendingTask {
             final UUID id;
             final UUID myJob;
-            final int d;
             final int[] replies;
             final ArrayList<Integer> myMonitors;
             int numReplies = 0;
             final String spec;
 
 
-            PendingTask(UUID id, UUID myJob, int d, String spec,  ArrayList<Integer> myMonitors){
+            PendingTask(UUID id, UUID myJob, String spec,  ArrayList<Integer> myMonitors){
                 this.id = id;
                 this.myJob = myJob;
-                this.d = d;
                 this.replies = new int[d];
                 this.myMonitors = myMonitors;
                 this.spec = spec;
